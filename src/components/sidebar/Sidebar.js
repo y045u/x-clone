e48b8f44c25d636b5ed5e52cec19a90754e81c86
y045u <fukasawa0405.tech@gmail.com>
2024-02-08 23:01:05 +0900

    ✨ Sidebarの雛形とアイコンを作成

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import XIcon from "@mui/icons-material/X";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* ツイッターアイコン */}
+      <XIcon className="sidebar--twitterIcon" />
+
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+
+      {/* ツイートボタン */}
+    </div>
+  );
+}
+
+export default Sidebar;
