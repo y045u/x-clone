860cd11fddff20f00c9161d180f68b55fb4cd903
y045u <fukasawa0405.tech@gmail.com>
2024-02-10 12:25:09 +0900

    ✨ create sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,5 +1,15 @@
 import React from "react";
 import XIcon from "@mui/icons-material/X";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { Button } from "@mui/material";
+import "./Sidebar.css";
 
 function Sidebar() {
   return (
@@ -8,14 +18,18 @@ function Sidebar() {
       <XIcon className="sidebar--twitterIcon" />
 
       {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
+      <SidebarOption text="home" Icon={HomeIcon} />
+      <SidebarOption text="explore" Icon={SearchIcon} />
+      <SidebarOption text="notifications" Icon={NotificationsIcon} />
+      <SidebarOption text="messages" Icon={MailOutlineIcon} />
+      <SidebarOption text="bookmarks" Icon={BookmarkIcon} />
+      <SidebarOption text="profile" Icon={AccountCircleIcon} />
+      <SidebarOption text="more" Icon={MoreHorizIcon} />
 
       {/* ツイートボタン */}
+      <Button variant="outlined" className="sidebar--tweet">
+        ポストする
+      </Button>
     </div>
   );
 }
