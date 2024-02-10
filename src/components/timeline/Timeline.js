f911459586c0d6a3805bf7cf363891d202beba94
y045u <fukasawa0405.tech@gmail.com>
2024-02-10 14:40:27 +0900

    ✨ 全体レイアウトの雛形作成

---
+++ src/components/timeline/Timeline.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Timeline() {
+  return (
+    <div className="timeline">
+      {/* Header */}
+      <div className="timeline">
+        <h2>ホーム</h2>
+      </div>
+
+      {/* TweetBox */}
+      <div className="Tweetbox">
+        <h2>ホーム</h2>
+      </div>
+
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      <div className="timeline">
+        <h2>ホーム</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Timeline;
