ab0720de8ed0face0cbde696dc050152660c4725
y045u <fukasawa0405.tech@gmail.com>
2024-02-10 15:24:41 +0900

    ✨ ホームのヘッダー作成

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,16 +1,17 @@
 import React from "react";
+import "./Timeline.css";
 
 function Timeline() {
   return (
     <div className="timeline">
       {/* Header */}
-      <div className="timeline">
+      <div className="timeline--header">
         <h2>ホーム</h2>
       </div>
 
       {/* TweetBox */}
       <div className="Tweetbox">
-        <h2>ホーム</h2>
+        <h2>box</h2>
       </div>
 
       {/* Post */}
@@ -23,8 +24,52 @@ function Timeline() {
       {/* Post */}
       {/* Post */}
       {/* Post */}
-      <div className="timeline">
-        <h2>ホーム</h2>
+      <div className="post">
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
+        <h2>pppppp</h2>
       </div>
     </div>
   );
